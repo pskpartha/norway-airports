473b73753f04c0dbc4377ece6e687faac0efc5aa
Partha Sarathi Kar <17142280+pskpartha@users.noreply.github.com>
2024-07-27 10:55:49 +0300

    Update environment.ts
    Key removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   airlabApiKey: '',
   apiBaseUrl: 'http://localhost:3000/',
   bingMapKey:
-    'AglmPyzD8PJVTQQwU90WeKBGL3Gpctl3q4xZ5oWa2T7REDQrPQdPfpDxAVIo1q7I',
+    '_ _ _ _',
 };
