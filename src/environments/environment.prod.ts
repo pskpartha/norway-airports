d565b4867863eff30390c29d05b21f48ca054c3c
Partha Sarathi Kar <17142280+pskpartha@users.noreply.github.com>
2024-07-27 10:54:53 +0300

    Update environment.prod.ts
    api key removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  airlabApiKey: 'df6b2557-a999-4c57-a44f-813870720b1a',
+  airlabApiKey: '_ _ _ _ _',
   apiBaseUrl: 'https://airlabs.co/api/v9/',
   bingMapKey:
-    'AglmPyzD8PJVTQQwU90WeKBGL3Gpctl3q4xZ5oWa2T7REDQrPQdPfpDxAVIo1q7I',
+    '_ _ _ _ _',
 };
